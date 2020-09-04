273f48471fbf106f28c1b0a2154e6f94f25ddd9a
HARSH HG GARG <harshgargthapar@gmail.com>
2020-09-04 20:59:31 +0530

    best code

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,47 +4,61 @@ using namespace std;
 
 //checking palindrome
 
-void check_palindrome(string *str1)
-{  
-    string copy=*str1;
-    string copyi=*str1;
-    int count=0;
-	char lastchar=0;
-	vector<char>v;
+// void check_palindrome(string *str1)
+// {  
+//     string copy=*str1;
+//     string copyi=*str1;
+//     int count=0;
+// 	char lastchar=0;
+// 	vector<char>v;
 
-	for (int i=0;i<copyi.size();i++)
-	{
-	    lastchar=copy.back();
-	    copy.pop_back();
-		v.push_back(lastchar);
-	}
+// 	for (int i=0;i<copyi.size();i++)
+// 	{
+// 	    lastchar=copy.back();
+// 	    copy.pop_back();
+// 		v.push_back(lastchar);
+// 	}
 	
-	for(int j=0;j<copyi.size();j++)
-	{
+// 	for(int j=0;j<copyi.size();j++)
+// 	{
 	  
 	
-	char check =copyi[j];
+// 	char check =copyi[j];
 
-		if(check==v[j])
+// 		if(check==v[j])
+// 		{
+// 			count++;
+// 			continue;
+// 		}
+// 		else
+// 		break;
+// 	}
+	
+	
+	
+// 	if(count==copyi.size())
+// 	cout<<"palindrome";
+// 	else
+// 	cout<<"notpaindrome";
+// }
+void checkpalindrome(string s1)
+{
+	int len=s1.length();
+	for(int i=0;i<len;i++)
+	{
+		if(s1[i]!=s1[len-i-1])
 		{
-			count++;
-			continue;
+			cout<<"string is not palindrome";
+			return;
 		}
-		else
-		break;
 	}
-	
-	
-	
-	if(count==copyi.size())
-	cout<<"palindrome";
-	else
-	cout<<"notpaindrome";
+	cout<<"string is palindrome";
 }
 int main()
 {
 	string str;
 	getline(cin,str);
-	check_palindrome(&str);
+//	check_palindrome(&str);//for check
+checkpalindrome(str);
 	return 0;
 }
